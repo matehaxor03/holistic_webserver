22947c32e0b19b361e2cc59484e2dec14daf378f
matehaxor03 <matehaxor03>
2022-12-10 11:27:15 +1100

    make error message payload generic to include http status

--- webserver/WebServer.go
+++ webserver/WebServer.go
@@ -181,9 +181,34 @@ func NewWebServer(port string, server_crt_path string, server_key_path string, q
 	}*/
 
 	write_response := func(w http.ResponseWriter, result json.Map, write_response_errors []error) {
+		keys := result.Keys()
+		
+		if len(keys) != 1 {
+			write_response_errors = append(write_response_errors, fmt.Errorf("number of root keys is incorrect"))
+		}
+		
 		if len(write_response_errors) > 0 {
-			result.SetNil("data")
-			result.SetErrors("[errors]", &write_response_errors)
+			inner_map_found := false
+			if len(keys) == 1 {
+				inner_map, inner_map_errors := result.GetMap(keys[0])
+				if inner_map_errors != nil {
+					write_response_errors = append(write_response_errors, inner_map_errors...)
+				} 
+				
+				if inner_map == nil {
+					write_response_errors = append(write_response_errors, fmt.Errorf("inner map is nil"))
+					inner_map_found = false
+				} else {
+					inner_map_found = true
+				}
+			}
+
+			if inner_map_found {
+				(result[keys[0]].(json.Map))["data"] = nil
+				(result[keys[0]].(json.Map))["[errors]"] = write_response_errors
+			} else {
+				result["unknown"] = json.Map{"data":nil, "[errors]":write_response_errors}
+			}
 		}
 
 		var json_payload_builder strings.Builder
@@ -192,10 +217,11 @@ func NewWebServer(port string, server_crt_path string, server_key_path string, q
 			write_response_errors = append(write_response_errors, result_as_string_errors...)
 		}
 		
+		w.Header().Set("Content-Type", "application/json")
 		if result_as_string_errors == nil {
 			w.Write([]byte(json_payload_builder.String()))
 		} else {
-			w.Write([]byte(fmt.Sprintf("{\"[errors]\":\"%s\", \"data\":null}", strings.ReplaceAll(fmt.Sprintf("%s", result_as_string_errors), "\"", "\\\""))))
+			w.Write([]byte(fmt.Sprintf("{\"unknown\":{\"[errors]\":\"%s\", \"data\":null}}", strings.ReplaceAll(fmt.Sprintf("%s", result_as_string_errors), "\"", "\\\""))))
 		}
 	}
 
